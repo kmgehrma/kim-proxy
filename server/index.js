ec7bc82a9e11cd20b556b5086cc46b21c0ed787e
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-06-15 19:24:46 -0500

    proxy

--- server/index.js
+++ server/index.js
@@ -14,7 +14,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.get('/:id', (req, res) => {
+const cacheOperation = (duration) => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url
+    let cachedBody = mcache.get(key);
+    if (cachedBody) {
+      res.send(cachedBody);
+      return;
+    } else {
+      res.sendResponse = res.send;
+      res.send = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body);
+      }
+      next();
+    }
+  }
+};
+
+app.get('/:id', cacheOperation(120), (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
 
